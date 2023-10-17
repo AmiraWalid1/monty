56ba48e706a4cd73d72b740dc924a3740903f5fb
11121997 <nawalkhaled27@yahoo.com>
2023-10-17 10:10:43 -0700

    update

---
+++ monty.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * main - Entry point
+ * @ac: arguments counter
+ * @av: argument vector
+ * Return: Always 0 (success)
+ */
+int main(int ac, char *av[])
+{
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	/*open file*/
+	return (0);
+}
