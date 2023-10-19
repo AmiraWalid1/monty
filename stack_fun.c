e7c0bed6216d0521ad7a5f32eca90e0eae189b4b
Amira Walid <amirawaleed012@gmail.com>
2023-10-19 15:03:27 +0300

    update

--- stack_fun.c
+++ stack_fun.c
@@ -18,6 +18,11 @@ void S_push(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 	new_node->n = atoi(arr[1]);
 	new_node->prev = NULL;
 	new_node->next = *stack;
