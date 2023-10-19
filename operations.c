04577770ac4feccebae3ca941773a7693321a37d
11121997 <nawalkhaled27@yahoo.com>
2023-10-19 15:31:21 -0700

    div

---
+++ operations.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * S_div - function that divides the second top element
+ * of the stack by the top element of the stack
+ * @stack: pointer to top node of the stack
+ * @line_number: line number
+ * Return: void
+ */
+void S_div(stack_t **stack, unsigned int line_number)
+{
+	int div;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack) = (*stack)->next;
+	div = (*stack)->n / (*stack)->prev->n;
+	(*stack)->n = div;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
