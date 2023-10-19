44cfffe2074fc1d77d5a1c606695f4766f832ae9
11121997 <nawalkhaled27@yahoo.com>
2023-10-19 15:36:57 -0700

    div

--- operations.c
+++ operations.c
@@ -16,6 +16,11 @@ void S_div(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	(*stack) = (*stack)->next;
 	div = (*stack)->n / (*stack)->prev->n;
 	(*stack)->n = div;
